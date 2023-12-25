cba07eb3d9bd5522c86c51a36a90693cab36ed8d
dev-abdulrhman <a.hwaij@outlook.com>
2023-12-25 09:12:06 +0300

    update file startUo

--- lib/Start/startUo.dart
+++ lib/Start/startUo.dart
@@ -26,10 +26,7 @@ class _StartUpState extends State<StartUp> {
             Text(
               text,
               textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontSize: 16,
-                  color: Color.fromRGBO(252, 255, 252, 1),
-                  fontFamily: "ReadexPro"),
+              style: TextStyle(fontSize: 16, color: Color.fromRGBO(252, 255, 252, 1), fontFamily: "ReadexPro"),
             )
           ],
         ));
@@ -48,10 +45,8 @@ class _StartUpState extends State<StartUp> {
                   width: double.infinity,
                   child: Stack(children: [
                     PageView(controller: _cont, children: [
-                      buildClipRRect("Agreement-bro.png",
-                          "نحن اختيارك اذا كان تبحث عن الامان في الصفقات"),
-                      buildClipRRect("Signing a contract-bro.png",
-                          "سهولة في توقيع العقود بين الطرفين"),
+                      buildClipRRect("Agreement-bro.png", "نحن اختيارك اذا كان تبحث عن الامان في الصفقات"),
+                      buildClipRRect("Signing a contract-bro.png", "سهولة في توقيع العقود بين الطرفين"),
                       ClipRRect(
                           borderRadius: BorderRadius.circular(8),
                           child: Column(
@@ -61,35 +56,16 @@ class _StartUpState extends State<StartUp> {
                                 margin: EdgeInsets.only(bottom: 20),
                                 child: Stack(
                                   children: [
-                                    Positioned(
-                                        child: Image.asset(
-                                            "images/House searching-bro.png",
-                                            height: 230,
-                                            width: 230)),
-                                    Positioned(
-                                        top: 15,
-                                        right: 70,
-                                        child: Image.asset(
-                                            "images/Horror video game-bro.png",
-                                            height: 230,
-                                            width: 230)),
-                                    Positioned(
-                                        top: 35,
-                                        left: 40,
-                                        child: Image.asset(
-                                            "images/City driver-rafiki.png",
-                                            height: 230,
-                                            width: 230)),
+                                    Positioned(child: Image.asset("images/House searching-bro.png", height: 230, width: 230)),
+                                    Positioned(top: 15, right: 70, child: Image.asset("images/Horror video game-bro.png", height: 230, width: 230)),
+                                    Positioned(top: 35, left: 40, child: Image.asset("images/City driver-rafiki.png", height: 230, width: 230)),
                                   ],
                                 ),
                               ),
                               Text(
                                 "كل ماتحتاجه لتاجير او استاجره متوفر في مكان واحد",
                                 textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    color: Color.fromRGBO(252, 255, 252, 1),
-                                    fontFamily: "ReadexPro"),
+                                style: TextStyle(fontSize: 16, color: Color.fromRGBO(252, 255, 252, 1), fontFamily: "ReadexPro"),
                               )
                             ],
                           ))
@@ -99,34 +75,23 @@ class _StartUpState extends State<StartUp> {
                       child: SmoothPageIndicator(
                         controller: _cont,
                         count: 3,
-                        effect: SlideEffect(
-                            dotColor: Color.fromRGBO(0, 119, 141, 1),
-                            activeDotColor: Color.fromRGBO(252, 255, 252, 1)),
+                        effect: SlideEffect(dotColor: Color.fromRGBO(0, 119, 141, 1), activeDotColor: Color.fromRGBO(252, 255, 252, 1)),
                       ),
                     )
                   ])),
             ),
             Container(
-              margin: EdgeInsets.only(top: 60),
-              decoration: BoxDecoration(
-                  color: Color.fromRGBO(114, 217, 247, 1),
-                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              margin: EdgeInsets.only(top: 80),
+              decoration: BoxDecoration(color: Color.fromRGBO(114, 217, 247, 1), borderRadius: BorderRadius.all(Radius.circular(20))),
               child: TextButton(
-                  style: ButtonStyle(
-                      padding: MaterialStatePropertyAll(
-                          EdgeInsets.symmetric(horizontal: 110, vertical: 8))),
+                  style: ButtonStyle(padding: MaterialStatePropertyAll(EdgeInsets.symmetric(horizontal: 110, vertical: 8))),
                   onPressed: () {
-                    Navigator.of(context)
-                        .pushNamedAndRemoveUntil("start", (route) => false);
+                    Navigator.of(context).pushNamedAndRemoveUntil("start", (route) => false);
                   },
                   child: Text(
                     "البدء",
                     textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromRGBO(252, 255, 252, 1),
-                        fontFamily: "ReadexPro"),
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Color.fromRGBO(252, 255, 252, 1), fontFamily: "ReadexPro"),
                   )),
             ),
           ]),
