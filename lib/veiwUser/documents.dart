613784e555b6d5a08b06e06592dbcf4f35ba442d
dev-abdulrhman <a.hwaij@outlook.com>
2024-01-10 17:20:52 +0300

    update Products file and Documents file

--- lib/veiwUser/documents.dart
+++ lib/veiwUser/documents.dart
@@ -35,9 +35,7 @@ class _DocumentsState extends State<Documents> {
           iconSize: 37,
           items: [
             FloatingNavbarItem(icon: Icons.home, title: "الرئيسية"),
-            FloatingNavbarItem(
-                icon: Icons.production_quantity_limits_rounded,
-                title: "منتجاتي"),
+            FloatingNavbarItem(icon: Icons.production_quantity_limits_rounded, title: "منتجاتي"),
             FloatingNavbarItem(
               icon: Icons.file_copy_rounded,
               title: "الوثائق",
@@ -47,11 +45,39 @@ class _DocumentsState extends State<Documents> {
           backgroundColor: const Color.fromRGBO(0, 39, 65, 1),
         ),
       ),
-      body: const Center(
-        child: Text(
-          "جاري العمل",
-          style: TextStyle(fontSize: 28),
-        ),
+      body: ListView(
+        children: [
+          Container(
+            height: 100,
+            width: double.infinity,
+            child: InkWell(
+              onTap: () {},
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                elevation: 7,
+                margin: EdgeInsets.all(10),
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 20, left: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'اسم العقد-رقمه',
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Icon(
+                        Icons.file_copy_rounded,
+                        size: 25,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/veiwUser/products.dart
+++ lib/veiwUser/products.dart
@@ -36,9 +36,7 @@ class _ProductsState extends State<Products> {
           iconSize: 37,
           items: [
             FloatingNavbarItem(icon: Icons.home, title: "الرئيسية"),
-            FloatingNavbarItem(
-                icon: Icons.production_quantity_limits_rounded,
-                title: "منتجاتي"),
+            FloatingNavbarItem(icon: Icons.production_quantity_limits_rounded, title: "منتجاتي"),
             FloatingNavbarItem(
               icon: Icons.file_copy_rounded,
               title: "الوثائق",
@@ -48,11 +46,121 @@ class _ProductsState extends State<Products> {
           backgroundColor: const Color.fromRGBO(0, 39, 65, 1),
         ),
       ),
-      body: const Center(
-        child: Text(
-          "جاري العمل",
-          style: TextStyle(fontSize: 28),
-        ),
+      body: ListView(
+        children: [
+          Container(
+            height: 170,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              elevation: 7,
+              margin: EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'اسم المنتج',
+                              style: TextStyle(
+                                fontSize: 26,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.end,
+                            ),
+                            Text(
+                              'وصف المنتج',
+                              style: TextStyle(
+                                fontSize: 26,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 5),
+                        child: Container(
+                          alignment: Alignment.center,
+                          height: 35,
+                          width: 75,
+                          child: TextButton(
+                            onPressed: () {},
+                            style: TextButton.styleFrom(
+                              backgroundColor: Color.fromRGBO(5, 162, 206, 1),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            child: Text(
+                              'تعديل',
+                              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: Text('السعر',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            )),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 5),
+                        child: Container(
+                          alignment: Alignment.center,
+                          height: 35,
+                          width: 75,
+                          child: TextButton(
+                            onPressed: () {},
+                            style: TextButton.styleFrom(
+                              backgroundColor: Color.fromRGBO(255, 86, 86, 1),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            child: Text(
+                              'حذف',
+                              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  //photo
+                  ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      bottomLeft: Radius.circular(15),
+                    ),
+                    child: Image.network(
+                      'https://wallpapers.com/images/featured-full/laptop-murjp1nk4lp1idlt.jpg',
+                      height: 160,
+                      width: 170,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
